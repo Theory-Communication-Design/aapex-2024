657c520eb9572420161e862357f1bfbbfb3b558d
kloizzo <kloizzo@LAPTOP-UIU259NP>
2024-11-04 15:41:45 -0500

    trying to fix video loop randomly stopping

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,23 @@
+import { useEffect } from 'react';
 import Link from 'next/link';
 
 export default function Landing() {
+  useEffect(() => {
+    const videoElement = document.querySelector('video');
+
+    const handlePlayback = () => {
+      if (videoElement.paused || videoElement.ended) {
+        videoElement.play();
+      }
+    };
+
+    videoElement.addEventListener('timeupdate', handlePlayback);
+
+    return () => {
+      videoElement.removeEventListener('timeupdate', handlePlayback);
+    };
+  }, []);
+
   return (
     <div className="relative flex flex-col items-center min-h-screen text-white pt-20">
       {/* Background Video */}
@@ -9,10 +26,10 @@ export default function Landing() {
         loop
         muted
         playsInline
+        preload="auto"
         poster="/video-posters/lockscreen-poster.jpg"
         className="absolute top-0 left-0 w-full h-full object-cover"
         style={{ zIndex: -1, aspectRatio: "16/9" }}
-        onEnded={(e) => e.target.play()}
       >
         <source src="./videos/lockscreen.mp4" type="video/mp4" />
         Your browser does not support the video tag.
